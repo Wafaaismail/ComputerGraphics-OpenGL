48526091c022c0e00a233b9be2544a61c6f56296
wafaa ismail <wafaaismail595@gmail.com>
2018-10-19 15:01:22 +0200

    Update thermodynamics.cpp

--- Third Law of Thermodyn/thermodynamics.cpp
+++ Third Law of Thermodyn/thermodynamics.cpp
@@ -16,36 +16,37 @@
 #include <time.h>
 #include <chrono>
 
-using namespace std; 
+using namespace std;
 /***********************************global variables******************************************/
 int degree = 4; //0......--//
 const int maxDegree = 5;
-const int moleNum = 30;
-const int moleNum2 = 30;
+const int moleNum = 60;
 Color tempColors[maxDegree];
 Molecular moleculars[moleNum];
-Molecular moleculars2[moleNum2];
 float _angle = -70.;
 /********************************prototypes************************************************/
 
 void molecularInit();
-void drawFilledCircle(GLfloat x, GLfloat y, GLfloat z, GLfloat radius);
-void resetColor();
 void drawMoles();
-void colorsInit();
-void drawQuad();
+
 void termometer();
+void termometerColorsInit();
+
+void drawFilledCircle(GLfloat x, GLfloat y, GLfloat z, GLfloat radius);
+void drawSphere(GLfloat x, GLfloat y, GLfloat z);
+
+void resetColor();
 void processSpecialKeys(int key, int mx, int my);
 void drawScene();
 void handleKeypress(unsigned char key, int x, int y);
 void initRendering();
 void handleResize(int w, int h);
-void drawSphere(GLfloat x, GLfloat y, GLfloat z);
 void update(int value);
+
 
 /************************************************************/
 int main(int argc, char** argv) {
-	
+
 	//Initialize GLUT
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
@@ -55,7 +56,7 @@ int main(int argc, char** argv) {
 	initRendering();
 
 	molecularInit();
-	colorsInit();
+
 	//Set handler functions
 	glutDisplayFunc(drawScene);
 	glutKeyboardFunc(handleKeypress);
@@ -70,13 +71,65 @@ int main(int argc, char** argv) {
 /********************************************Defination*****************************************/
 void molecularInit() {
 
-	for (int i = 0; i < moleNum; i++)
+	for (float i = 0; i < moleNum; i++)
 	{
-		moleculars[i].Randomize();
+		moleculars[(int)i].randmize();
 		//cout << i << "\t" << moleculars[i].posX << "\t" << moleculars[i].posY << "\t" << moleculars[i].posZ << endl;
+		//cout << i / moleNum << "\t" << 1 - (i / moleNum) << endl;
+		moleculars[(int)i].color.setColors(i / moleNum, 1 - (i / moleNum), 1);
 	}
 
 }
+
+void drawMoles() {
+	molecularInit();
+	resetColor();
+	glPushMatrix();
+	glTranslatef(0, 0.5, 0);
+	glColor3f(0, 0, 0);
+	for (int i = 0; i < moleNum; i++) {
+		glColor3f(moleculars[i].color.red, moleculars[i].color.green, moleculars[i].color.blue);
+		drawSphere(moleculars[i].posX, moleculars[i].posY, moleculars[i].posZ);
+		resetColor();
+		if (degree != 0) {
+			glRotatef(_angle, 1.0f, 1.0f, 1.0f);
+			Sleep(maxDegree - degree);
+		}
+	}
+	glPopMatrix();
+}
+
+
+
+void termometer() {
+
+	termometerColorsInit();
+	glPushMatrix();
+	//Add ambient light
+	GLfloat ambientColor[] = { 1.f, 1.f, 1.f, 1.0f };
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientColor);
+	glTranslatef(-6, -1.5, 0.);
+	glBegin(GL_QUADS);
+	glColor3f(tempColors[degree].red, tempColors[degree].green, tempColors[degree].blue);
+	glVertex3f(2.5, 0.2, -5.);
+	glVertex3f(2.7, 0.2, -5.);
+	glVertex3f(2.7, 3, -5.);
+	glVertex3f(2.5, 3, -5.);
+	glEnd();
+	drawFilledCircle(2.6, .1, -5., .15);
+	glPopMatrix();
+
+
+}
+
+void termometerColorsInit() {
+	for (double i = 0; i < maxDegree; i += 1) { // i >> degree
+		cout << i / maxDegree << "\t" << 1 - (i / maxDegree) << endl;
+		tempColors[(int)i].setColors(i / maxDegree, 0, 1 - (i / maxDegree));
+	}
+}
+
+
 
 void drawFilledCircle(GLfloat x, GLfloat y, GLfloat z, GLfloat radius) {
 	int i;
@@ -96,138 +149,6 @@ void drawFilledCircle(GLfloat x, GLfloat y, GLfloat z, GLfloat radius) {
 	glEnd();
 }
 
-void resetColor() {
-	glColor3f(1, 1, 1);
-}
-
-void drawMoles() {
-	resetColor();
-	glPushMatrix();
-	glTranslatef(0, 0.5, 0);
-	glColor3f(0, 0, 0);
-	for (int i = 0; i < moleNum; i++) {
-		glColor3f(1, 0, 0);
-		drawSphere(moleculars[i].posX, moleculars[i].posY, moleculars[i].posZ);
-		resetColor();
-		glColor3f(0, 1, 0);
-		drawSphere(moleculars2[i].posX+1,.5+ moleculars2[i].posY, .5+moleculars2[i].posZ);
-
-		if (degree != 0) {
-			glRotatef(_angle, 1.0f, 1.0f, 1.0f);
-			Sleep(maxDegree-degree);
-		}
-
-	}
-	glPopMatrix();
-}
-
-void colorsInit() {
-	for (double i = 0; i < maxDegree; i+=1) { // i >> degree
-		cout << i / maxDegree << "\t" << 1 - (i / maxDegree) << endl;
-		tempColors[(int)i].setColors(i/maxDegree  , 0, 1 - (i/maxDegree));
-	}
-}
-
-void handleKeypress(unsigned char key, int x, int y) {
-	switch (key) {
-	case 27: //Escape key
-		exit(0);
-	}
-}
-
-//Initializes 3D rendering
-void initRendering() {
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_COLOR_MATERIAL);
-
-	glEnable(GL_LIGHTING); //Enable lighting
-
-	glEnable(GL_LIGHT0); //Enable light #0
-	glEnable(GL_LIGHT1); //Enable light #1
-
-	glEnable(GL_NORMALIZE); //Automatically normalize normals
-	glShadeModel(GL_SMOOTH); //Enable smooth shading
-
-}
-
-//Called when the window is resized
-void handleResize(int w, int h) {
-	glViewport(0, 0, w, h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(45.0, (double)w / (double)h, 1.0, 200.0);
-}
-
-void drawQuad() {
-
-	glBegin(GL_QUADS);
-	glColor3f(0.6, 0, 0.2);
-	glVertex3f(-2, -1, -5.);
-	glVertex3f(-1.5, 0., -5.);
-	glVertex3f(1.5, 0., -5.);
-	glVertex3f(0.5, -1., -5.);
-
-	glEnd();
-}
-
-void termometer() {
-
-	glPushMatrix();
-	//Add ambient light
-	GLfloat ambientColor[] = { 1.f, 1.f, 1.f, 1.0f }; 
-	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientColor);
-	glTranslatef(-6, -1.5, 0.);
-	glBegin(GL_QUADS);
-	glColor3f(tempColors[degree].red, tempColors[degree].green, tempColors[degree].blue);
-	glVertex3f(2.5, 0.2, -5.);
-	glVertex3f(2.7, 0.2, -5.);
-	glVertex3f(2.7, 3, -5.);
-	glVertex3f(2.5, 3, -5.);
-	glEnd();
-	drawFilledCircle(2.6, .1, -5., .15);
-	glPopMatrix();
-
-
-}
-
-void processSpecialKeys(int key, int mx, int my) {
-
-	switch (key) {
-
-	case GLUT_KEY_UP: //increase temp
-		(degree == maxDegree-1) ? degree = maxDegree - 1  : degree += 1;
-		cout << degree << endl;
-		glutPostRedisplay();
-		break;
-	case GLUT_KEY_DOWN: //decrease temp
-		(degree == 0) ? degree = 0 : degree -= 1;
-		cout << degree << endl;
-		glutPostRedisplay();
-		break;
-
-	default:
-		break;
-	}
-
-
-}
-//Draws the 3D scene
-void drawScene() {
-
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	//glClearColor(1.0f, 1.0f, 204.0 / 255, 1.0f);
-	glMatrixMode(GL_MODELVIEW); //Switch to the drawing perspective
-	glLoadIdentity(); //Reset the drawing perspective
-	termometer();
-	//resetColor();
-	glTranslatef(0., 0., -10.);
-	if (degree!=0)   glRotatef(_angle, 0.0f, 1.0f, 1.0f);	
-	//drawSphere();
-	
-	drawMoles();
-	//drawQuad();
-	glutSwapBuffers(); 
-}
 void drawSphere(GLfloat x, GLfloat y, GLfloat z) {
 
 	//Add ambient light
@@ -263,6 +184,79 @@ void drawSphere(GLfloat x, GLfloat y, GLfloat z) {
 	glutSolidSphere(.5, 20., 20.);
 
 }
+
+
+
+void resetColor() {
+	glColor3f(1, 1, 1);
+}
+
+void processSpecialKeys(int key, int mx, int my) {
+
+	switch (key) {
+
+	case GLUT_KEY_UP: //increase temp
+		(degree == maxDegree - 1) ? degree = maxDegree - 1 : degree += 1;
+		cout << degree << endl;
+		glutPostRedisplay();
+		break;
+	case GLUT_KEY_DOWN: //decrease temp
+		(degree == 0) ? degree = 0 : degree -= 1;
+		cout << degree << endl;
+		glutPostRedisplay();
+		break;
+
+	default:
+		break;
+	}
+
+
+}
+
+void handleKeypress(unsigned char key, int x, int y) {
+	switch (key) {
+	case 27: //Escape key
+		exit(0);
+	}
+}
+
+void drawScene() {
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW); //Switch to the drawing perspective
+	glLoadIdentity(); //Reset the drawing perspective
+	termometer();
+	
+
+	glTranslatef(0., 0., -10.);
+	if (degree != 0)   glRotatef(_angle, 0.0f, 1.0f, 1.0f);
+
+	drawMoles();
+	glutSwapBuffers();
+}
+//Initializes 3D rendering
+void initRendering() {
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_COLOR_MATERIAL);
+
+	glEnable(GL_LIGHTING); //Enable lighting
+
+	glEnable(GL_LIGHT0); //Enable light #0
+	glEnable(GL_LIGHT1); //Enable light #1
+
+	glEnable(GL_NORMALIZE); //Automatically normalize normals
+	glShadeModel(GL_SMOOTH); //Enable smooth shading
+
+}
+//Called when the window is resized
+void handleResize(int w, int h) {
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, (double)w / (double)h, 1.0, 200.0);
+}
+
 void update(int value) {
 	_angle += 1.5f;
 	if (_angle > 360) {
